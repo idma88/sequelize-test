d8579b49385fe87c71b792bfb069ffa15fcb083d
Dmitrii Ivanov <idma88@yandex.ru>
2019-10-03 21:08:28 +0300

    Create/update dataset

---
+++ src/datasets/games.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { id: 227300, titleShort:"ETS2", titleFull: "Euro Truck Simulator 2" }, 
+  { id: 270880, titleShort:"ATS", titleFull:  "American Truck Simulator" }, 
+]
---
+++ src/datasets/social_networks.js
@@ -0,0 +1,14 @@
+module.exports = [
+  { id: 1, title: "TruckersMP" },
+  { id: 2, title: "World of Trucks" },
+  { id: 3, title: "Steam" },
+  { id: 4, title: "uPlay" },
+  { id: 5, title: "Discord" },
+  { id: 6, title: "ВКонтакте" },
+  { id: 7, title: "Facebook" },
+  { id: 8, title: "Одноклассники" },
+  { id: 9, title: "Instagram" },
+  { id: 10, title: "Youtube" },
+  { id: 11, title: "Twitch" },
+  { id: 12, title: "GitHub" },
+]
